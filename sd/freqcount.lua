1233dd03faa7a05e76d0bf452cf901b94bc4b779
Jacob Christ <jacob@pontech.com>
2024-01-11 06:24:43 -0800

    Added averaging and increased font size.

--- sd/freqcount.lua
+++ sd/freqcount.lua
@@ -17,6 +17,7 @@ EDGE_Count = 0
 EDGE_Last = 0
 EDGE_This = 0
 PERIOD = 0;
+PERIOD_AVERAGE = 0;
 
 function MyInterrupt(pin_no)
 	EDGE_This = os.clock()
@@ -29,6 +30,7 @@ function MyInterrupt(pin_no)
 		else
 			PERIOD = EDGE_Delta
 		end
+		PERIOD_AVERAGE = PERIOD_AVERAGE * 0.2 + PERIOD * 0.8
 		EDGE_Count = 0
 	else
 		EDGE_Count = EDGE_Count + 1
@@ -38,31 +40,36 @@ end
 
 
 function MainFunction()
+	local background_color = ez.RGB(0, 0, 0)
 	ez.SetPinInp(FREQ_Pin,true,false) -- GPIO: DIGITAL PULSE (FREQ) IN: CONFIGURES THE I/O PIN AS DISCRETE INPUT, (PULL-UP) & PULL-DOWN BOTH DISABLED (DEFAULT)
 	-- Assign on change interrupt to pin 1
 	ez.SetPinIntr(FREQ_Pin, "MyInterrupt", 1) -- 1 = Rising edge only
 
-	ez.Cls(ez.RGB(0,0,0))
-	ez.SetColor(ez.RGB(0,0,255))
-	ez.SetXY(0,0)
-	print(string.format("EarthLCD Frequency Counter Example"))
+	ez.Cls(ez.RGB(0, 0, 0))
+	ez.BoxFill(0, 0, ez.Width, 40, ez.RGB(64,64,64)) -- X1, Y1, X2, Y2, Color
+
+	ez.SetFtFont(6,18,18)
+	ez.SetColor(ez.RGB(255,255,255))
+	ez.SetXY(0,10)
+	print(string.format(" EarthLCD Frequency Counter"))
 
 	while (1) do
-		local color = 64
 		local x1 = 0
-		local x2 = 200
-		local y1 = 20
-		local y2 = 50
+		local x2 = ez.Width
+		local y1 = 90
+		local y2 = y1 +100
 
-		ez.BoxFill(x1,y1, x2,y2, ez.RGB(color,color,color)) -- X1, Y1, X2, Y2, Color
+		ez.BoxFill(x1,y1, x2,y2, ez.RGB(0, 0, 0)) -- X1, Y1, X2, Y2, Color
 
-		ez.SetXY(x1,y1+3)
-		ez.SetColor(ez.RGB(255,255,255))
+		ez.SetXY(x1,y1+20)
+		ez.SetColor(ez.RGB(255,255,0))
+		print(string.format("   FREQENCY %0.2f Hz", 1/PERIOD_AVERAGE))
+		ez.SetColor(ez.RGB(0,255,255))
+		print(string.format("   PERIOD %f sec", PERIOD_AVERAGE))
+		ez.SetColor(ez.RGB(255,0,255))
 		print(string.format("   EDGE_Count %d", EDGE_Count))
-		print(string.format("   PERIOD %f", PERIOD))
-		print(string.format("   FREQENCY %f", 1/PERIOD))
 
-		--ez.Wait_ms(250)
+		--ez.Wait_ms(250) -- Addeing this delay prevents measurements of periods less than the delay period
 	end
 	
 end
